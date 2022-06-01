30d456262ac4d802a255abab7c3a811c5582303d
Abhishek-Chachadi <53827557+Abhishek-Chachadi@users.noreply.github.com>
2022-06-01 22:34:07 +0530

    Update Customer.java
    one to many

--- src/main/java/CMCModel/Customer.java
+++ src/main/java/CMCModel/Customer.java
@@ -1,18 +1,32 @@
 package CMCModel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
-@Data
 @Entity
 @Table
 public class Customer {
+
+	public Customer() {
+
+	}
 
 	public Customer(String firstName, String lastName, int age, String sex, String address) {
 		super();
@@ -41,5 +55,17 @@ public class Customer {
 
 	@Column(nullable = false)
 	private String address;
+	
+	@OneToMany(mappedBy = "patients",cascade={CascadeType.ALL})
+	@JsonIgnore
+	private List<CheckUp> checkUp;
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return id;
+		
+	}
+	
 
 }
+
