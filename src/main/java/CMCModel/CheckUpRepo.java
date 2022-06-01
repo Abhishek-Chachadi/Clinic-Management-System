666a0cec26bf3ae3f2d5d895a7ccb366bc63206f
Abhijit-98 <mabhijit.ec17@rvce.edu.in>
2022-06-01 22:52:58 +0530

    Added CustomerRepo Named Query

--- src/main/java/CMCModel/CheckUpRepo.java
+++ src/main/java/CMCModel/CheckUpRepo.java
@@ -3,5 +3,6 @@ package CMCModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CheckUpRepo extends JpaRepository<CheckUp, Long> {
+	
 
 }
--- src/main/java/CMCModel/CustomerRepo.java
+++ src/main/java/CMCModel/CustomerRepo.java
@@ -1,7 +1,10 @@
 package CMCModel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
+	@Query(value = "select * from Customer where id = ?1",nativeQuery = true)
+	Customer findByCustomerName(Long patientsID);
 
 }
